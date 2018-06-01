1dab24edbe5fa1dd77d02e7cac2845682b916519
James Strachan <james.strachan@gmail.com>
2018-06-01 14:21:51 +0100

    use different app id and index name

--- themes/gohugoioTheme/src/js/docsearch.js
+++ themes/gohugoioTheme/src/js/docsearch.js
@@ -1,7 +1,8 @@
 var docsearch = require('docsearch.js/dist/cdn/docsearch.js');
 docsearch({
   apiKey: '929044fb1a3a337a4eb4108c7769f269',
-  indexName: 'jenkins-x.io',
+  appId: "TCRDI09Q5C",
+  indexName: 'jxd',
   inputSelector: '#search-input',
   debug: true // Set debug to true if you want to inspect the dropdown
 });
