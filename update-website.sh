eed57fa1cdfd5e501e8cd71790eb4cb09556b852
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-04-02 21:47:36 +0100

    fixed: update website script

--- update-website.sh
+++ update-website.sh
@@ -3,7 +3,7 @@
 pushd jenkins-x-website
   jx step git credentials
   git config credential.helper store
-  git add -A
+  git add *
   git commit --allow-empty -a -m "updated site"
   git push origin
 popd
