a6c488a6028fd0ceba6f73cd87198376c4cb153c
Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>
2020-10-24 01:47:21 +0530

    feat: add ability to enlarge image on click
    fixes #3089

---
+++ static/apidocs/zoom.js
@@ -0,0 +1,9 @@
+import mediumZoom from 'medium-zoom'
+
+mediumZoom('.data-zoomable', {
+    margin: 24,
+    background: '#BADA55',
+    scrollOffset: 0,
+    container: '#zoom-container',
+    template: '#zoom-template',
+  })
