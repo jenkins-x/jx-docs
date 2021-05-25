d5e9c0902a92ee8e4adcfa41e3d8c68a1a1edadf
James Strachan <james.strachan@gmail.com>
2021-05-25 15:55:28 +0100

    fix: regen the schema

--- scripts/ci/update-website.sh
+++ scripts/ci/update-website.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+git clone https://github.com/jenkins-x/jx-api
+cp jx-api/schema/core.jenkins-x.io/v4beta1/requirements.json jenkins-x-website/schemas/jx-requirements.json
+
 pushd jenkins-x-website
   jx step git credentials
   git config credential.helper store
